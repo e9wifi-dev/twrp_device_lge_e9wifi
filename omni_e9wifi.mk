6c17f17b88a62651848bdd830da1346c4613405d
darknightmare <rubentkd98@gmail.com>
2015-05-26 22:08:13 +0200

    e9wifi: Initial TWRP commit for android-5.1

---
+++ omni_e9wifi.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/lge/e9wifi/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := e9wifi
+PRODUCT_NAME := omni_e9wifi
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-V700
+PRODUCT_MANUFACTURER := LGE
